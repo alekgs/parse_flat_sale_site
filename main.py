6bbf5f5013d435dd344ad7e500a682d5365d0741
Alex Sayapin <alekgs@ya.ru>
2025-03-01 13:54:47 +0700

    updating main.py

--- main.py
+++ main.py
@@ -5,38 +5,43 @@ import requests
 LINK = 'https://gorno-altaysk.cian.ru/cat.php'
 
 # Запрос для поиска (можно запрашивать на вводе)
+
 REGION = 4719  # Республика Алтай, Горно-Алтайск
-MIN_PRICE = 5000000
-MAX_PRICE = 8000000
-MIN_AREA = 30
-MAX_AREA = 80
+MIN_PRICE = 5000000  # минимальная цена
+MAX_PRICE = 8000000  # максимальная цена
+MIN_AREA = 30  # минимальная площадь
+MAX_AREA = 80  # максимальная площадь
 ROOM1 = 1  # 1 комн.
 ROOM2 = 1  # 2 комн.
-# ROOM3 = 1 # 3 комн.
+# ROOM3 = 1         # 3 комн и т.д.
 
-## headers (прикидываемся браузером Chrome)
+## headers для get-запроса
 HEADERS = {'Content-Type': 'text/html; charset=utf-8',
            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) '
                          'AppleWebKit/537.36 (KHTML, like Gecko) '
                          'Chrome/133.0.0.0 Safari/537.36'}
 
-# Данные для запроса
-DATA = {'currency': 2,
-        'deal_type': 'sale',
-        'engine_version': 2,
-        'offer_type': 'flat',
-        'minprice': MIN_PRICE,
-        'maxprice': MAX_PRICE,
-        'region': REGION,
-        'mintarea': MIN_AREA,
-        'maxtarea': MAX_AREA,
-        'room1': ROOM1,
-        'room2': ROOM2,
-        # 'room3': ROOM3,
-        }
+# формируем данные для get-запроса
+DATA = {
+		'deal_type': 'sale',
+		'engine_version': 2,
+		'offer_type': 'flat',
+		'minprice': MIN_PRICE,
+		'maxprice': MAX_PRICE,
+		'region': REGION,
+		'mintarea': MIN_AREA,
+		'maxtarea': MAX_AREA,
+		'room1': ROOM1,
+		'room2': ROOM2,
+		}
 
 
-def get_info_from_site(link, data, headers):
+def get_info_from_site(link: str, data: str, headers: str) -> str:
+	"""
+	Функция отправляет get-запрос, анализирует ответ от сайта и возвращает
+	результат
+	"""
+
 	# посылаем GET запрос на сервер
 	response = requests.get(LINK, params=DATA, headers=HEADERS)
 
@@ -46,69 +51,67 @@ def get_info_from_site(link, data, headers):
 	if status_code == 200:
 		print('Server: 200 OK')
 		get_parsing_data(response)
-		return
+
 	print("Server error: ", status_code)
 	exit(1)
 
 
 def get_parsing_data(response):
-		### далее парсим результат
-		soup = LxmlSoup(response.text)
+	### далее парсим результат
+	soup = LxmlSoup(response.text)
 
-		# ищем заголовок запроса
-		title_result = soup.find_all('h1',
-		                             class_='_93444fe79c--color_text-primary'
-		                                    '-default--vSRPB _93444fe79c--'
-		                                    'lineHeight_36px--K6dvk _93444fe79c'
-		                                    '--fontWeight_bold--BbhnX _93444fe79c'
-		                                    '--fontSize_28px--P1gR4 _93444fe79c'
-		                                    '--display_block--KYb25 _93444fe79c'
-		                                    '--text--e4SBY _93444fe79c'
-		                                    '--text_letterSpacing__normal'
-		                                    '--tfToq'
-		                             )
+	# ищем заголовок запроса
+	title_result = soup.find_all('h1',
+	                             class_='_93444fe79c--color_text-primary'
+	                                    '-default--vSRPB _93444fe79c--'
+	                                    'lineHeight_36px--K6dvk _93444fe79c'
+	                                    '--fontWeight_bold--BbhnX _93444fe79c'
+	                                    '--fontSize_28px--P1gR4 _93444fe79c'
+	                                    '--display_block--KYb25 _93444fe79c'
+	                                    '--text--e4SBY _93444fe79c'
+	                                    '--text_letterSpacing__normal'
+	                                    '--tfToq'
+	                             )
 
-		# ищем имя класса с количеством полученных записей
-		flats_count = soup.find_all('h5',
-		                            class_='_93444fe79c--color_text-primary-'
-		                                   'default--vSRPB _93444fe79c--'
-		                                   'lineHeight_20px--fX7_V _93444fe79c'
-		                                   '--fontWeight_bold--BbhnX '
-		                                   '_93444fe79c--fontSize_14px--reQMB '
-		                                   '_93444fe79c--display_block--KYb25 '
-		                                   '_93444fe79c--text--e4SBY _93444fe79c'
-		                                   '--text_letterSpacing__normal--tfToq'
-		                            )
+	# ищем имя класса с количеством полученных записей
+	flats_count = soup.find_all('h5',
+	                            class_='_93444fe79c--color_text-primary-'
+	                                   'default--vSRPB _93444fe79c--'
+	                                   'lineHeight_20px--fX7_V _93444fe79c'
+	                                   '--fontWeight_bold--BbhnX '
+	                                   '_93444fe79c--fontSize_14px--reQMB '
+	                                   '_93444fe79c--display_block--KYb25 '
+	                                   '_93444fe79c--text--e4SBY _93444fe79c'
+	                                   '--text_letterSpacing__normal--tfToq'
+	                            )
 
-		print(title_result[0].text())
-		print(flats_count[0].text())
+	print(title_result[0].text())
+	print(flats_count[0].text())
 
-		# Описание
-		offer_title = soup.find_all('span',
-		                            class_='_93444fe79c--color_text-primary-'
-		                                   'default--vSRPB _93444fe79c--'
-		                                   'lineHeight_28px--KFXmc _93444fe79c'
-		                                   '--fontWeight_bold--BbhnX _93444fe79c'
-		                                   '--fontSize_22px--sFuaL _93444fe79c'
-		                                   '--display_block--KYb25 _93444fe79c'
-		                                   '--text--e4SBY _93444fe79c--'
-		                                   'text_letterSpacing__normal--tfToq'
-		                            )
+	# Описание
+	offer_title = soup.find_all('span',
+	                            class_='_93444fe79c--color_text-primary-'
+	                                   'default--vSRPB _93444fe79c--'
+	                                   'lineHeight_28px--KFXmc _93444fe79c'
+	                                   '--fontWeight_bold--BbhnX _93444fe79c'
+	                                   '--fontSize_22px--sFuaL _93444fe79c'
+	                                   '--display_block--KYb25 _93444fe79c'
+	                                   '--text--e4SBY _93444fe79c--'
+	                                   'text_letterSpacing__normal--tfToq'
+	                            )
 
-		# for i, title in enumerate(offer_title):
-		# 	t = title.text()
-		# 	price = soup.find_all('span', class_='')[i].text()
-		# 	print(f'{i} - {t}, {price}')
+	# for i, title in enumerate(offer_title):
+	# 	t = title.text()
+	# 	price = soup.find_all('span', class_='')[i].text()
+	# 	print(f'{i} - {t}, {price}')
 
+	# ссылки на найденные квартиры
+	links = soup.find_all('a', class_='_93444fe79c--link--VtWj6')
 
-
-		# ссылки на найденные квартиры
-		links = soup.find_all('a', class_='_93444fe79c--link--VtWj6')
-
-		for i, link in enumerate(links):
-			url = link.get('href')
-			# price = soup.find('span', class_ ='').text()
-			print(f'{i} - {url}')
+	for i, link in enumerate(links):
+		url = link.get('href')
+		# price = soup.find('span', class_ ='').text()
+		print(f'{i} - {url}')
 
 
 if __name__ == "__main__":
